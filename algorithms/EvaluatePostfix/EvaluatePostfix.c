0e8219bfb4c5db86107c86beb99ad4255229cce1
Anurag Anand <anuraganand789@hotmail.com>
2020-07-15 01:10:56 +0530

    Evaluate Postfix Operation

---
+++ algorithms/EvaluatePostfix/EvaluatePostfix.c
@@ -0,0 +1,57 @@
+#include "EvaluatePostfix.h"
+
+void main(int argCount, char **arguments){
+    
+    char *postfixExpression = *++arguments;
+    printf("%s -> ", postfixExpression);
+
+    unsigned char noOfOperands = 0;
+
+    for(; *postfixExpression != '\0'; ++postfixExpression) if(isOperand(postfixExpression)) ++noOfOperands;
+
+    unsigned short *stackOfOperands = (unsigned short *) calloc(noOfOperands, sizeof(unsigned short));
+    char            topOfStack = -1;
+
+    for(postfixExpression = *arguments; *postfixExpression != '\0'; ++postfixExpression){
+        if(isOperand(postfixExpression)) *(stackOfOperands + ++topOfStack) = alphaToInt(postfixExpression);
+	else {
+	    unsigned short *operand1 = (stackOfOperands + topOfStack--);
+	    unsigned short *operand2 = (stackOfOperands + topOfStack--);
+	    *(stackOfOperands + ++topOfStack) = performOperation(postfixExpression, operand1, operand2);
+	}
+    }
+
+    printf("%d\n", *(stackOfOperands + topOfStack));
+
+    free(stackOfOperands);
+}
+
+unsigned short alphaToInt(char *operand){
+    switch(*operand){
+        default : return EOF;
+	case '0': return 0;
+	case '1': return 1;
+	case '2': return 2;
+	case '3': return 3;
+	case '4': return 4;
+	case '5': return 5;
+	case '6': return 6;
+	case '7': return 7;
+	case '8': return 8;
+	case '9': return 9;
+    }
+}
+
+unsigned short performOperation(char *operator, unsigned short *operand1, unsigned short *operand2){
+    switch(*operator){
+        default : return -1;
+	case '-' : return *operand1 - *operand2;
+	case '+' : return *operand1 + *operand2;
+	case '*' : return *operand1 * *operand2;
+	case '/' : return *operand1 / *operand2;
+    }
+}
+
+bool isOperand(char *charachter) {
+    return *charachter >= '0' && *charachter <= '9';
+}
---
+++ algorithms/EvaluatePostfix/EvaluatePostfix.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+bool isOperand(char *);
+unsigned short performOperation(char *, unsigned short *, unsigned short *);
+unsigned short alphaToInt(char *);
