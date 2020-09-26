c52e2561e0210baeb38cafe83d3c5d85fab00cbf
Anurag Anand <anuraganand789@hotmail.com>
2020-09-26 15:47:27 +0530

    implementation for strcpy function from string.h

---
+++ utils/StrCpy.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+char * strcpyImpl_1(char * restrict destinationString, char * restrict sourceString){
+    if(NULL == destinationString || NULL == sourceString) return NULL;
+    int const destinationStringLength = strlen(destinationString);
+    int const sourceStringLength      = strlen(sourceString);
+
+    if(destinationStringLength < sourceStringLength) return NULL;
+    
+    char * newString = (char *) malloc(sizeof(char) * (destinationStringLength + 1));
+    int i = 0;
+    //Copy source string
+    for(;i < destinationStringLength && i < sourceStringLength; ++i) 
+        newString[i] = sourceString[i];
+
+    --i;
+    //Copy remanis of destination string
+    while(i <= destinationStringLength) {
+	printf("copying Char %c -- from dest to new string \n", destinationString[i]);
+        newString[i] = destinationString[i++];
+	printf("copied Char %c -- from dest to new string \n", newString[i]);
+    }
+
+    newString[destinationStringLength] = '\0';
+
+    return newString;
+}
+
+char * strcpyImpl_2(char * restrict destinationString, char * restrict sourceString){
+    if(NULL == destinationString || NULL == sourceString) return NULL;
+    int const destinationStringLength = strlen(destinationString);
+    int const sourceStringLength      = strlen(sourceString);
+
+    if(destinationStringLength < sourceStringLength) return NULL;
+    
+    char * newString = (char *) malloc(sizeof(char) * (destinationStringLength + 1));
+    
+    char * charIterator = newString;
+    while((*charIterator++ = *sourceString++) != '\0');
+
+    int i = sourceStringLength;
+    for(; i < destinationStringLength; ++i){ newString[i] = destinationString[i]; }
+
+    newString[destinationStringLength] = '\0';
+
+    return newString;
+}
+
+char * strcpyImpl_3(char * restrict destinationString, char * restrict sourceString){
+    if(NULL == destinationString || NULL == sourceString) return NULL;
+    int const destinationStringLength = strlen(destinationString);
+    int const sourceStringLength      = strlen(sourceString);
+
+    if(destinationStringLength < sourceStringLength) return NULL;
+    
+    char * newString = (char *) malloc(sizeof(char) * (destinationStringLength + 1));
+    char * charIterator = newString;
+    //copy the destination string
+    while((*charIterator++ = *destinationString++) != '\0');
+
+    charIterator = newString;
+    //copy the source string
+    while((*charIterator++ = *sourceString++) != '\0');
+    
+    //remove null value put by source string
+    if(destinationStringLength > sourceStringLength) newString[sourceStringLength] = destinationString[sourceStringLength];
+
+    return newString;
+}
+
+int main(int argc, char ** const argv){
+    char * firstName = "My First name";
+    printf("1. First Name Variable :- %s\n", firstName);
+    firstName        = strcpyImpl_3(firstName, "My Last ");
+    //firstName        = strcpyImpl_2(firstName, "My Last ");
+    //firstName        = strcpyImpl_1(firstName, "My Last ");
+    printf("2. First Name Variable :- %s\n", firstName);
+    printf("Length of new string %d\n", (int) strlen(firstName));
+    if(NULL != firstName && *firstName != '\0') free(firstName);
+    return 0;
+}
