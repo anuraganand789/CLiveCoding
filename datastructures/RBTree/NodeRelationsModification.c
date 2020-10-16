d50d31068ba344dc9207ca0bf6df87d7de8cadbc
Anurag Anand <anuraganand789@hotmail.com>
2020-10-16 15:17:40 +0530

    comment added to clarify, why I am not using 'siblingOf' utility function

--- datastructures/RBTree/NodeRelationsModification.c
+++ datastructures/RBTree/NodeRelationsModification.c
@@ -10,6 +10,9 @@ struct RBNode *leftNodeReplacesTheParent(struct RBNode * const rbNode){
   struct RBNode * parent;
 
   if(notNull(rbNode) && notNull(parent = parentOf(rbNode))) {
+    //We can also use siblingOf function over here , but we already know
+    // that the current child is the left child of it's parent
+    // so, we can easily get it's sibling by just getting the right child of it's parent
     struct RBNode * const sibling      = rightChildOf(parent);
     struct RBNode * const grandParent  = parentOf(parent);
 
@@ -41,8 +44,11 @@ struct RBNode *rightNodeReplacesTheParent(struct RBNode * const rbNode){
   struct RBNode * parent;
   if(notNull(rbNode) && notNull(parent = parentOf(rbNode))){
 
-    struct RBNode * const grandParent = parentOf(parent);
+    //We can also use siblingOf function over here , but we already know
+    // that the current child is the right child of it's parent
+    // so, we can easily get it's sibling by just getting the left child of it's parent
     struct RBNode * const sibling     = leftChildOf(parent);
+    struct RBNode * const grandParent = parentOf(parent);
     
     struct RBNode * const leftChildOfCurrentNode = leftChildOf(rbNode);
     
