b36d70a3ec1b6303fabaeb3cdb95d06a4f30413c
Anurag Anand <anuraganand789@hotmail.com>
2020-10-16 15:02:26 +0530

    new functions for replacing parent with it's child i.e left and right rotations

---
+++ datastructures/RBTree/NodeRelationsModification.c
@@ -0,0 +1,69 @@
+#include "RB.h"
+
+//         P                                       C
+//       /  \                                    /   \
+//      C   RP      ===========>               LC     P
+//    /   \                                         /  \
+//  LC     RC                                      RC  RP
+
+struct RBNode *leftNodeReplacesTheParent(struct RBNode * const rbNode){
+  struct RBNode * parent;
+
+  if(notNull(rbNode) && notNull(parent = parentOf(rbNode))) {
+    struct RBNode * const sibling      = rightChildOf(parent);
+    struct RBNode * const grandParent  = parentOf(parent);
+
+    struct RBNode * const rightChildOfCurrNode = rightChildOf(rbNode);
+    
+    if(notNull(grandParent)) {
+      if(isLeftChildOf(grandParent, parent)) makeLeftChildOf(grandParent, rbNode);
+      else makeRightChildOf(grandParent, rbNode);
+    }
+
+    makeParentOf(rbNode, grandParent);
+    makeParentOf(parent, rbNode);
+
+    makeLeftChildOf(parent, rightChildOfCurrNode);
+    makeRightChildOf(rbNode, parent);
+  }
+  return rbNode;
+}
+
+
+//         P                         C
+//	  / \                      /   \
+//       S   C       ====>        P    RC
+//	    / \                  / \
+//	   LC RC                S  LC
+//
+
+struct RBNode *rightNodeReplacesTheParent(struct RBNode * const rbNode){
+  struct RBNode * parent;
+  if(notNull(rbNode) && notNull(parent = parentOf(rbNode))){
+
+    struct RBNode * const grandParent = parentOf(parent);
+    struct RBNode * const sibling     = leftChildOf(parent);
+    
+    struct RBNode * const leftChildOfCurrentNode = leftChildOf(rbNode);
+    
+    if(notNull(grandParent)){
+      if(isLeftChildOf(grandParent, parent)) makeLeftChildOf(grandParent, rbNode);
+      else makeRightChildOf(grandParent, rbNode);
+    }
+
+    makeParentOf(rbNode, grandParent);
+    makeParentOf(parent, rbNode);
+
+    makeLeftChildOf(rbNode, parent);
+    makeRightChildOf(parent, leftChildOfCurrentNode);
+  }
+  return rbNode;
+}
+
+
+
+//extern struct RBNode *leftNodeReplacesTheGrandParent(struct RBNode * const rbNode);
+//extern struct RBNode *rightNodeReplacesTheGrandParent(struct RBNode * const rbNode);
+
+
+
