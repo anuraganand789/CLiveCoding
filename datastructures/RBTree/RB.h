82bd66fbbfb2779dc3249b606421873a6283cd27
Anurag Anand <anuraganand789@hotmail.com>
2020-10-18 23:41:42 +0530

    changs made to inline the functions

--- datastructures/RBTree/RB.h
+++ datastructures/RBTree/RB.h
@@ -12,30 +12,11 @@ struct RBNode {
   struct RBNode *right;
 };
 
-// Conditional Functions
+//Conditionals 
 inline bool null(struct RBNode * const rbNode)       { return rbNode == NULL; }
 inline bool notNull(struct RBNode * const rbNode)    { return rbNode != NULL; }
-inline bool itIsARedNode(struct RBNode * rbNode)     { return notNull(rbNode) && rbNode->color == RED; }
-inline bool itIsABlackNode(struct RBNode * rbNode)   { return null(rbNode) || rbNode->color == BLACK;  }
-inline int dataOf(struct RBNode * const rbNode)      { return notNull(rbNode) ? rbNode->data : EOF;    }
 
-extern bool isLeftChildOf(struct RBNode *rootNode, struct RBNode *rbNode);
-extern bool isRightChildOf(struct RBNode *rootNode, struct RBNode *rbNode);
-bool hasLeftChild(struct RBNode * const rbNode) { return notNull(leftChildOf(rbNode)); }
-bool hasRightChild(struct RBNode * const rbNode) { return notNull(rightChildOf(rbNode)); }
-bool hasParent(struct RBNode * const rbNode) { return notNull(parentOf(rbNode)); }
-extern char *nodeColorAsString(struct RBNode * const rbNode);
-
-// For Debug purposes
-extern void printRBNode(struct RBNode * const root);
-extern void printRBTree(struct RBNode * const root, int const noOfRepeats, char const indentChar);
-
-extern void *debugMalloc(size_t const sizeOfMalloc);
-extern void debugFree(void * const pointer);
-
-//Relations
-extern struct RBNode *uncleOf(struct RBNode * const root);
-extern struct RBNode *siblingOf(struct RBNode * const root);
+//Relations 
 inline struct RBNode *parentOf(struct RBNode * const root) { 
     return notNull(root) ? root->parent : NULL; 
 }
@@ -48,6 +29,30 @@ inline struct RBNode *leftChildOf(struct RBNode *root) {
 inline struct RBNode *rightChildOf(struct RBNode *root) { 
     return notNull(root) ? root->right : NULL; 
 }
+// Conditional Functions
+extern bool isLeftChildOf(struct RBNode *rootNode, struct RBNode *rbNode);
+extern bool isRightChildOf(struct RBNode *rootNode, struct RBNode *rbNode);
+
+inline bool itIsARedNode(struct RBNode * rbNode)     { return notNull(rbNode) && rbNode->color == RED; }
+inline bool itIsABlackNode(struct RBNode * rbNode)   { return null(rbNode) || rbNode->color == BLACK;  }
+inline int dataOf(struct RBNode * const rbNode)      { return notNull(rbNode) ? rbNode->data : EOF;    }
+
+inline bool hasLeftChild(struct RBNode * const rbNode) { return notNull(leftChildOf(rbNode)); }
+inline bool hasRightChild(struct RBNode * const rbNode) { return notNull(rightChildOf(rbNode)); }
+inline bool hasParent(struct RBNode * const rbNode) { return notNull(parentOf(rbNode)); }
+
+extern char *nodeColorAsString(struct RBNode * const rbNode);
+
+// For Debug purposes
+extern void printRBNode(struct RBNode * const root);
+extern void printRBTree(struct RBNode * const root, int const noOfRepeats, char const indentChar);
+
+extern void *debugMalloc(size_t const sizeOfMalloc);
+extern void debugFree(void * const pointer);
+
+//Relations
+extern struct RBNode *uncleOf(struct RBNode * const root);
+extern struct RBNode *siblingOf(struct RBNode * const root);
 
 //Set Relations
 
@@ -65,8 +70,8 @@ inline void makeRightChildOf(struct RBNode * const parentNode, struct RBNode * c
 }
 
 //Rotations or Node Relation Modification
-extern struct RBNode *leftChildReplacesTheGrandParent(struct RBNode * const rbNode);
-extern struct RBNode *rightChildReplacesTheGrandParent(struct RBNode * const rbNode);
+extern struct RBNode *leftGrandChildReplacesTheGrandParent(struct RBNode * const rbNode);
+extern struct RBNode *rightGrandChildReplacesTheGrandParent(struct RBNode * const rbNode);
 
 extern struct RBNode *leftChildReplacesTheParent(struct RBNode * const rbNode);
 extern struct RBNode *rightChildReplacesTheParent(struct RBNode * const rbNode);
