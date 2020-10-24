fe026af1c9c4d612adf2262f83109eefee825caa
Anurag Anand <anuraganand789@hotmail.com>
2020-10-24 23:16:41 +0530

    Code cleanup

--- utils/StrCat.c
+++ utils/StrCat.c
@@ -3,35 +3,37 @@
 #include<string.h>
 
 char * strcatImpl_1(char * restrict firstString, char * restrict secondString){
-    int const noOfCharsInFirstString = firstString == NULL ? 0 : strlen(firstString);
-    int const noOfCharsInSecondString = secondString == NULL ? 0 : strlen(secondString);
+    int const noOfCharsInFirstString  = firstString  ? strlen(firstString)  : 0;
+    int const noOfCharsInSecondString = secondString ? strlen(secondString) : 0;
     
-    if(0 == noOfCharsInFirstString && 0 == noOfCharsInSecondString) return NULL;
+    if(noOfCharsInFirstString && noOfCharsInSecondString) {
+        char * const newString = (char *) 
+                                  (
+				    !noOfCharsInFirstString 
+				    ? malloc(sizeof(char) * (noOfCharsInSecondString + 1))
+            		            : !noOfCharsInSecondString 
+				        ? malloc(sizeof(char) * (noOfCharsInFirstString + 1))
+            		                : malloc(sizeof(char) * (noOfCharsInFirstString + noOfCharsInSecondString + 1))
+            		          );
 
-    char * const newString = (char *) 
-                              (noOfCharsInFirstString == 0
-                                 ? malloc(sizeof(char) * (noOfCharsInSecondString + 1))
-			         : noOfCharsInSecondString == 0 
-			         ? malloc(sizeof(char) * (noOfCharsInFirstString + 1))
-			         : malloc(sizeof(char) * (noOfCharsInFirstString + noOfCharsInSecondString + 1))
-			      );
-
-    if(NULL == newString) return NULL;
-
-    char * iterator = newString;
-    if(0 != noOfCharsInFirstString) { 
-        while((*iterator++ = *firstString++) != '\0'); 
-        if(0 != noOfCharsInSecondString) --iterator;
+        char * iterator = newString;
+        if(noOfCharsInFirstString) { 
+            while((*iterator++ = *firstString++)); 
+	    //move one block back to start adding new chars from second string by overwriting the null char
+            if(noOfCharsInSecondString) --iterator;
+        }
+        
+        if(noOfCharsInSecondString) { while((*iterator++ = *secondString++)); }
+        return newString;
     }
-    
-    if(0 != noOfCharsInSecondString) { while((*iterator++ = *secondString++) != '\0'); }
 
-    return newString;
+    return NULL;
+
 }
 
 int main(int argc, char** argv){
-    char* const slogan = strcatImpl_1("Jai!!!", "Shree Ram");
+    char * const slogan = strcatImpl_1("Jai!!!", "Shree Ram");
     printf("=> %s \n", slogan);
-    if(NULL != slogan && '\0' != *slogan) free(slogan);
+    if(slogan && *slogan) free(slogan);
     return 0;
 }
