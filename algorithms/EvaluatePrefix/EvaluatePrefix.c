19b4e48abada204146a78a0c5443554e088d64d5
Anurag Anand <anuraganand789@hotmail.com>
2020-11-01 21:19:56 +0530

    replaced a switch case block with 9 cases.. with a simple ternary operator

--- algorithms/EvaluatePrefix/EvaluatePrefix.c
+++ algorithms/EvaluatePrefix/EvaluatePrefix.c
@@ -7,19 +7,7 @@ bool isOperator(char *ch){
 }
 
 int alphaToInt(char *ch) {
-    switch(*ch){
-        default : return EOF;
-	case '0' : return 0;
-	case '1' : return 1;
-	case '2': return 2;
-	case '3': return 3;
-	case '4': return 4;
-	case '5': return 5;
-	case '6' : return 6;
-	case '7' : return 7;
-	case '8' : return 8;
-	case '9' : return 9;
-    }
+    return (*ch >= '0' && *ch <= '9') ?  *ch - '0' : EOF;
 }
 short calc(char *ops, short *opd1, short *opd2) {
     switch(*ops){
